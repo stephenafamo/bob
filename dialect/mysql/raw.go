55a18274764b16c261493ecd8dd494cd972761f5
Stephen Afam-Osemene <me@stephenafamo.com>
2022-07-27 17:18:24 +0100

    add initial support for mysql

---
+++ dialect/mysql/raw.go
@@ -0,0 +1,10 @@
+package mysql
+
+import (
+	"github.com/stephenafamo/bob/expr"
+	"github.com/stephenafamo/bob/query"
+)
+
+func Raw(q string, args ...any) query.BaseQuery[expr.Raw] {
+	return expr.RawQuery(dialect, q, args...)
+}
