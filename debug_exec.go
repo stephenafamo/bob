822877194330660bd763c80629a0b36090248e32
Stephen Afam-Osemene <me@stephenafamo.com>
2022-12-26 09:25:24 +0100

    Specify print destination for DebugExecutor

--- debug_exec.go
+++ debug_exec.go
@@ -4,32 +4,41 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/stephenafamo/scan"
 )
 
-func DebugExecutor(q Executor) Executor {
-	return debugQueryer{w: q}
+// DebugExecutor wraps an existing [Executor] and writes all
+// queries and args to the given [io.Writer]
+// if w is nil, it fallsback to [os.Stdout]
+func DebugExecutor(exec Executor, w io.Writer) Executor {
+	if w == nil {
+		w = os.Stdout
+	}
+	return debugQueryer{w: w, exec: exec}
 }
 
 type debugQueryer struct {
-	w Executor
+	w    io.Writer
+	exec Executor
 }
 
 func (d debugQueryer) print(query string, args ...any) {
-	fmt.Println(query)
+	fmt.Fprintln(d.w, query)
 	for i, arg := range args {
-		fmt.Printf("%d: %#v\n", i, arg)
+		fmt.Fprintf(d.w, "%d: %#v\n", i, arg)
 	}
-	fmt.Printf("\n")
+	fmt.Fprintf(d.w, "\n")
 }
 
 func (d debugQueryer) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
 	d.print(query, args...)
-	return d.w.ExecContext(ctx, query, args...)
+	return d.exec.ExecContext(ctx, query, args...)
 }
 
 func (d debugQueryer) QueryContext(ctx context.Context, query string, args ...any) (scan.Rows, error) {
 	d.print(query, args...)
-	return d.w.QueryContext(ctx, query, args...)
+	return d.exec.QueryContext(ctx, query, args...)
 }
