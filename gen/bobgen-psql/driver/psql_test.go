7f4c5518a22a93760f3a7015a597d52e46a5d3e1
Stephen Afam-Osemene <me@stephenafamo.com>
2023-01-14 21:34:05 +0100

    Use driver specific env variable for test dsn

--- gen/bobgen-psql/driver/psql_test.go
+++ gen/bobgen-psql/driver/psql_test.go
@@ -23,12 +23,12 @@ var testDB string
 var (
 	flagOverwriteGolden = flag.Bool("overwrite-golden", false, "Overwrite the golden file with the current execution results")
 
-	dsn = os.Getenv("DRIVER_TEST_DSN")
+	dsn = os.Getenv("PSQL_TEST_DSN")
 )
 
 func TestAssemble(t *testing.T) {
 	if dsn == "" {
-		t.Fatalf("No environment variable DRIVER_TEST_DSN")
+		t.Fatalf("No environment variable PSQL_TEST_DSN")
 	}
 	// somehow create the DB
 	config, err := pgx.ParseConfig(dsn)
