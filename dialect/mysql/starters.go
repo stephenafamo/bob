cc4d830b84a0faa006042d735e2e7213d11481c6
Stephen Afam-Osemene <me@stephenafamo.com>
2023-01-17 19:45:48 +0100

    add comments to starters

--- dialect/mysql/starters.go
+++ dialect/mysql/starters.go
@@ -10,6 +10,15 @@ type Expression = dialect.Expression
 //nolint:gochecknoglobals
 var bmod = expr.Builder[Expression, Expression]{}
 
+// X is a flexible starter that joins the given expressions with a space
+func X(exp any, others ...any) Expression {
+	return bmod.X(exp, others...)
+}
+
+// F creates a function expression with the given name and args
+//
+//	SQL: generate_series(1, 3)
+//	Go: psql.F("generate_series", 1, 3)
 func F(name string, args ...any) *dialect.Function {
 	f := dialect.NewFunction(name, args...)
 
@@ -20,54 +29,75 @@ func F(name string, args ...any) *dialect.Function {
 	return &f
 }
 
+// S creates a string literal
+// SQL: 'a string'
+// Go: psql.S("a string")
 func S(s string) Expression {
 	return bmod.S(s)
 }
 
-func X(exp any, others ...any) Expression {
-	return bmod.X(exp, others...)
-}
-
+// SQL: NOT true
+// Go: psql.Not("true")
 func Not(exp any) Expression {
 	return bmod.Not(exp)
 }
 
+// SQL: a OR b OR c
+// Go: psql.Or("a", "b", "c")
 func Or(args ...any) Expression {
 	return bmod.Or(args...)
 }
 
+// SQL: a AND b AND c
+// Go: psql.And("a", "b", "c")
 func And(args ...any) Expression {
 	return bmod.And(args...)
 }
 
+// SQL: a || b || c
+// Go: psql.Concat("a", "b", "c")
 func Concat(args ...any) Expression {
 	return bmod.X(expr.Join{Exprs: args, Sep: " || "})
 }
 
+// SQL: $1, $2, $3
+// Go: psql.Args("a", "b", "c")
 func Arg(args ...any) Expression {
 	return bmod.Arg(args...)
 }
 
+// SQL: ($1, $2, $3)
+// Go: psql.ArgGroup("a", "b", "c")
 func ArgGroup(args ...any) Expression {
 	return bmod.ArgGroup(args...)
 }
 
-func P(exp any) Expression {
-	return bmod.P(exp)
-}
-
+// SQL: $1, $2, $3
+// Go: psql.Placeholder(3)
 func Placeholder(n uint) Expression {
 	return bmod.Placeholder(n)
 }
 
-func Raw(query string, args ...any) Expression {
-	return bmod.Raw(query, args...)
+// SQL: (a and b)
+// Go: psql.P("a and b")
+func P(exp any) Expression {
+	return bmod.P(exp)
 }
 
+// SQL: (a, b)
+// Go: psql.Group("a", "b")
 func Group(exps ...any) Expression {
 	return bmod.Group(exps...)
 }
 
+// SQL: "table"."column"
+// Go: psql.Quote("table", "column")
 func Quote(ss ...string) Expression {
 	return bmod.Quote(ss...)
+}
+
+// SQL: where a = $1
+// Go: psql.Raw("where a = ?", "something")
+func Raw(query string, args ...any) Expression {
+	return bmod.Raw(query, args...)
 }
