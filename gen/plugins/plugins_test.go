b6ac9c63735cd946b3663b08399bf0e82c3e4280
Stephen Afam-Osemene <me@stephenafamo.com>
2025-08-12 00:54:26 +0100

    Fix error in assertion

--- gen/plugins/plugins_test.go
+++ gen/plugins/plugins_test.go
@@ -5,11 +5,13 @@ import "github.com/stephenafamo/bob/gen"
 var (
 	_ gen.StatePlugin[any]            = &enumsPlugin[any, any, any]{}
 	_ gen.DBInfoPlugin[any, any, any] = &enumsPlugin[any, any, any]{}
-	_ gen.StatePlugin[any]            = modelsPlugin[any]{}
-	_ gen.StatePlugin[any]            = factoryPlugin[any]{}
-	_ gen.StatePlugin[any]            = &queriesOutputPlugin[any, any, any]{}
-	_ gen.DBInfoPlugin[any, any, any] = &queriesOutputPlugin[any, any, any]{}
-	_ gen.StatePlugin[any]            = dbErrorsPlugin[any]{}
-	_ gen.StatePlugin[any]            = joinsPlugin[any]{}
-	_ gen.StatePlugin[any]            = loadersPlugin[any]{}
+
+	_ gen.StatePlugin[any] = modelsPlugin[any]{}
+	_ gen.StatePlugin[any] = factoryPlugin[any]{}
+	_ gen.StatePlugin[any] = dbErrorsPlugin[any]{}
+	_ gen.StatePlugin[any] = joinsPlugin[any]{}
+	_ gen.StatePlugin[any] = loadersPlugin[any]{}
+
+	_ gen.StatePlugin[any]                  = &queriesOutputPlugin[any, any, any]{}
+	_ gen.TemplateDataPlugin[any, any, any] = &queriesOutputPlugin[any, any, any]{}
 )
