d422c5455ac31c7f57ec9774e89f63cbfbec26f3
Eli Ribble <eli@theribbles.org>
2025-09-16 16:48:35 +0000

    Add tools for golangci-lint to shell.nix

--- shell.nix
+++ shell.nix
@@ -3,5 +3,13 @@
 pkgs.mkShell {
 	buildInputs = [
 		pkgs.go
+		pkgs.golangci-lint
 	];
+	shellHook = ''
+		# Install noctx if not already installed
+		go install github.com/sonatard/noctx/cmd/noctx@latest
+
+		# Add Go binaries to PATH
+		export PATH=$PATH:$(go env GOPATH)/bin
+	'';
 }
